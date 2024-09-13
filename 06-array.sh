d81410772faeb722465fc7f2617cbe35dffd6f14
YuganderReddy <yugander243@gmail.com>
2024-09-14 01:54:56 +0530

    varaibles

--- 06-array.sh
+++ 06-array.sh
@@ -3,8 +3,8 @@
 # index starts from 0, size is 3
 FRUITS=("APPLE" "KIWI" "ORANGE") #Array
 
-echo "First fruit is: {$FRUITS[0]}"
-echo "Second fruit is: {$FRUITS[1]}"
-echo "Third fruit is: {$FRUITS[2]}"
+echo "First fruit is: $FRUITS[0]"
+echo "Second fruit is: $FRUITS[1]"
+echo "Third fruit is: $FRUITS[2]"
 
-echo "All fruits are: {$FRUITS[@]}"
+echo "All fruits are: $FRUITS[@]"
