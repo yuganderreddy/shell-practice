f88e6a8e4e0ab45e8ab2cb1f79551f1538ebe92c
YuganderReddy <yugander243@gmail.com>
2024-09-14 01:50:35 +0530

    varaibles

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,3 +4,8 @@ echo "please enter your username::"
 
 read -s USERNAME #interrupts us and takes input into this USERNAME variable & we can use it anywhere.
 echo "Entered username is:: $USERNAME"
+
+echo "please enter your password::"
+
+read -s PASSWORD #interrupts us and takes input into this PASSWORD variable & we can use it anywhere.
+echo "Entered password is:: $PASSWORD"
