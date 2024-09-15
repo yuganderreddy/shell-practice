ae07e877ccef69dedb706cd7840b9700e2b98c87
YuganderReddy <yugander243@gmail.com>
2024-09-15 13:39:55 +0530

    installations

--- 11-installations.sh
+++ 11-installations.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#installation of GIT using shell-script.
+
 USERID=$(id -u)
 
 echo "userid is :$USERID"
+
+if [ $? -ne 0 ]
+then
+    echo "Please run the script with Root privileges..."
+fi
