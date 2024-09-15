8669a4eafe3084cf95c80e5aac3a74334e9ff21a
YuganderReddy <yugander243@gmail.com>
2024-09-15 13:51:01 +0530

    installations

--- 11-installations.sh
+++ 11-installations.sh
@@ -10,3 +10,12 @@ if [ $USERID -ne 0 ]
 then
     echo "Please run the script with Root privileges..."
 fi
+
+dnf list installed git -y
+ if [ $? -ne 0 ]
+ then
+    echo "git is not installed, it is going to install....."
+    dnf install git -y
+else 
+    echo "git is already installed...."
+fi    
