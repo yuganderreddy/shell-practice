7fd5da1232f0d83363f8604f33455b9374ab295d
YuganderReddy <yugander243@gmail.com>
2024-09-15 14:01:45 +0530

    installations

--- 11-installations.sh
+++ 11-installations.sh
@@ -14,9 +14,16 @@ fi
 
 dnf list installed git -y
  if [ $? -ne 0 ]
- then
+    then
     echo "git is not installed, it is going to install....."
     dnf install git -y
-else 
+    if [ $? -ne 0 ]
+        then
+        echo "git installation is not successfull, please check it...."
+        exit 1
+        else
+        echo "git installation is successfull...."
+    fi
+    else 
     echo "git is already installed...."
 fi    
