e6ac264b729786483f5fee6f664e8b08606255e3
YuganderReddy <yugander243@gmail.com>
2024-09-18 00:53:44 +0530

    shell

--- 17_redirectors.sh
+++ 17_redirectors.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+LOG_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOG_FOLDER
+
 USERID=$(id -u)
 
 # for color codes.
@@ -11,7 +17,7 @@ N="\e[0m"
 CHECK_ROOT() {
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R please run the script with root privileges $N"
+        echo -e "$R please run the script with root privileges $N" &>> LOG_FILE
         exit 1
     fi
     
@@ -20,15 +26,27 @@ CHECK_ROOT() {
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$R $2 is not installed successfully $N"
+        echo -e "$R $2 is not installed successfully $N" &>> LOG_FILE
         exit 1
     else
-        echo -e "$G $2 is installed successfully $N" 
+        echo -e "$G $2 is installed successfully $N" &>> LOG_FILE
     fi
 
 }
 
+USAGE() {
+    echo -e "$R USAGE:: $N sudo sh 17-redirectors.sh package1 package2 ..." &>> LOG_FILE
+    exit 1
+}
+
+echo "Script started executing at: $(date)" &>> LOG_FILE
+
 CHECK_ROOT
+
+if [ $# -eq 0 ]
+then
+    USAGE
+fi
 
 # sh 15-loops.sh git mysql postfix nginx
 for package in $@   #$@ refers to all the variables/args passed to it.
@@ -36,11 +54,11 @@ do
     dnf list installed $package
     if [ $? -ne 0 ]
     then
-        echo "$package is not installed, going to install it..."
+        echo "$package is not installed, going to install it..." &>> LOG_FILE
         dnf install $package -y
         VALIDATE $? "installing $package"
     else
-        echo "$package is installed, no worries..."
+        echo "$package is installed, no worries..." &>> LOG_FILE
     fi
     
 done
