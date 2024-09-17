006a609d07874b724bcb8e353916b69d3c47871a
YuganderReddy <yugander243@gmail.com>
2024-09-18 01:04:36 +0530

    shell

--- 17_redirectors.sh
+++ 17_redirectors.sh
@@ -17,7 +17,7 @@ N="\e[0m"
 CHECK_ROOT() {
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R please run the script with root privileges $N" &>> LOG_FILE
+        echo -e "$R please run the script with root privileges $N" &>>$LOG_FILE
         exit 1
     fi
     
@@ -26,20 +26,20 @@ CHECK_ROOT() {
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$R $2 is not installed successfully $N" &>> LOG_FILE
+        echo -e "$R $2 is not installed successfully $N" &>>$LOG_FILE
         exit 1
     else
-        echo -e "$G $2 is installed successfully $N" &>> LOG_FILE
+        echo -e "$G $2 is installed successfully $N" &>>$LOG_FILE
     fi
 
 }
 
 USAGE() {
-    echo -e "$R USAGE:: $N sudo sh 17-redirectors.sh package1 package2 ..." &>> LOG_FILE
+    echo -e "$R USAGE:: $N sudo sh 17-redirectors.sh package1 package2 ..." &>>$LOG_FILE
     exit 1
 }
 
-echo "Script started executing at: $(date)" &>> LOG_FILE
+echo "Script started executing at: $(date)" &>>$LOG_FILE
 
 CHECK_ROOT
 
@@ -54,11 +54,11 @@ do
     dnf list installed $package
     if [ $? -ne 0 ]
     then
-        echo "$package is not installed, going to install it..." &>> LOG_FILE
+        echo "$package is not installed, going to install it..." &>>$LOG_FILE
         dnf install $package -y
         VALIDATE $? "installing $package"
     else
-        echo "$package is installed, no worries..." &>> LOG_FILE
+        echo "$package is installed, no worries..." &>>$LOG_FILE
     fi
     
 done
