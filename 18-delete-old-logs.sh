4b424e2555810f78cc609ba5912b0f6d7676ad2b
YuganderReddy <yugander243@gmail.com>
2024-09-21 22:59:56 +0530

    shell

---
+++ 18-delete-old-logs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+SOURCE_DIR=/home/ec2-user/logs
+
+if [ -d $SOURCE_DIR]
+then
+    echo "$SOURCE_DIR exists"
+else
+    echo "$SOURCE_DIR not exists"
+    exit 1
+fi
