4b49aeb996b01ca92103a68af2517d413ccc6ef5
YuganderReddy <yugander243@gmail.com>
2024-09-23 16:48:25 +0530

    shell

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -3,7 +3,7 @@
 DISK_USAGE= $(df -hT | grep xfs)
 DISK_THRESOLD=5 #In Real Projects, usually it is 75.
 
-while IFS= read -r file     #IFS= Internal Field Seperator here IFS is empty it means it will not ignore white spaces, -r =not to ignore special characters like /
+while IFS= read -r line     #IFS= Internal Field Seperator here IFS is empty it means it will not ignore white spaces, -r =not to ignore special characters like /
 do
     echo $file
     echo $line
